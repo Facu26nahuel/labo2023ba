3114436449a7dea72857d9ed40e863072eed5f05
Facu26nahuel <facundo26nahuel@gmail.com>
2023-09-14 02:46:37 +0000

    exp1.0_0

--- src/workflow-inicial/601_RUN_correr_workflow_1_0.r
+++ src/workflow-inicial/601_RUN_correr_workflow_1_0.r
@@ -15,6 +15,6 @@ source("~/labo2023ba/src/workflow-inicial/621_DR_corregir_drifting_1_0.r")
 source("~/labo2023ba/src/workflow-inicial/631_FE_historia_1_0.r")
 source("~/labo2023ba/src/workflow-inicial/641_TS_training_strategy_1_0.r")
 
-# ultimos pasos, muy lentos
+# ultimos pasos, muy lentos.
 source("~/labo2023ba/src/workflow-inicial/651_HT_lightgbm_1_0.r")
 source("~/labo2023ba/src/workflow-inicial/661_ZZ_final_1_0.r")

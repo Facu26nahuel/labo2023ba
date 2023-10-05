150007a4f2004784f89ac688ab2314ca4d46edfb
facundo26nahuel <facundo26nahuel@gmail.com>
2023-10-04 21:33:41 -0300

    654

--- src/workflow-inicial/Final_661_ZZ_final_v1.r
+++ src/workflow-inicial/Final_661_ZZ_final_v1.r
@@ -35,7 +35,7 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ6610_final"
+PARAM$experimento <- "ZZ6610_final_v1"
 PARAM$exp_input <- "HT6510_final"
 
 # Atencion, que cada modelos se procesa con 5 semillas, ajuste a SUS necesidades
